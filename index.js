8cd280ae6075a073209847e4f16bf3bc66afa9b6
Anthony Basco <abasco2@outlook.com>
2024-10-15 08:06:30 -0500

    Add files via upload

---
+++ index.js
@@ -0,0 +1,20 @@
+console.log("This is my first Javascript Program");
+
+const MyName = "Anthony Basco";
+console.log(MyName);
+
+const Message =
+  " You have received this message because you have been chosen to open an important vault. Here is the secret combination: 10 40 39";
+console.log(Message);
+
+//This is the message you will receive
+let str1 =
+  " You have received this message because you have been chosen to open an important vault. Here is the secret combination:";
+//pseudocode lock 5 * 2 = 10
+let str2 = 5 * 2;
+//pseudocode lock 80 / 2 = 40
+let str3 = 80 / 2;
+//pseudocode lock 30 + 9 = 39
+let str4 = 30 + 9;
+// This is the full popup message
+alert(str1 + " " + str2 + " " + str3 + " " + str4);
